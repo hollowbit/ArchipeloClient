5a52c2ab010d3f168f73cedc79fa6ec834771a7d
Landon <Landon@DESKTOP-4EUJS0V>
2017-06-09 20:38:25 -0400

    Added various profession resource materials and artwork.
    Added names and descriptions to existing and new items.
    
    Adjusted animation speed for various weapons (Further adjusting will
    likely be required.)
    
    Added quest items.

--- core/src/net/hollowbit/archipelo/ArchipeloClient.java
+++ core/src/net/hollowbit/archipelo/ArchipeloClient.java
@@ -62,7 +62,7 @@ public class ArchipeloClient extends ApplicationAdapter {
 	public static float STATE_TIME = 0;//this is for looping animations where it doesn't matter where it starts.
 	public static boolean DEBUGMODE = false;
 	public static boolean INVERT = false;
-	public static boolean PLACEHOLDER_ART_MODE = DEBUGMODE;
+	public static boolean PLACEHOLDER_ART_MODE = DEBUGMODE = true;
 	public static boolean CINEMATIC_MODE = false;
 	public static boolean SHOW_COLLISION_RECTS = false;
 	
